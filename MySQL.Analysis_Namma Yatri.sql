fe4fed3ee6be50b9102b64afea9709f66c3cbd65
Nidish09 <140280163+Nidish09@users.noreply.github.com>
2023-10-03 21:06:46 +0530

    Update MySQL.Analysis_Namma Yatri.sql

--- MySQL.Analysis_Namma Yatri.sql
+++ MySQL.Analysis_Namma Yatri.sql
@@ -27,7 +27,7 @@ select avg(fare) from trips
 # 9. distance travelled
 select sum(distance) from trips
 
-# 10. which is the most used payment method? 
+# 10. which is the most used payment method ? 
 select a.method from payment a inner join
 (select faremethod, count(tripid) from trips
 group by faremethod
@@ -35,61 +35,61 @@ order by count(tripid) desc
 limit 1) b
 on a.id = b.faremethod
 
-# 11. the highest payment was made through which instrument
+# 11. the highest payment was made through which instrument ?
 select a.method from payment a inner join
 (select * from trips
 order by fare desc limit 1) b
 on a.id = b.faremethod
 
-# 12, which two locations had the most trips
+# 12, which two locations had the most trips ?
 select loc_from, loc_to, count(loc_to) from trips
 group by loc_from, loc_to
 order by count(loc_to) desc
 limit 2
 
-# 13. top 5 earning drivers
+# 13. top 5 earning drivers ?
 select driverid, sum(fare) from trips
 group by driverid
 order by sum(fare) desc
 limit 5
 
-# 14. which duration had more trips
+# 14. which duration had more trips ?
 select duration, count(distinct(tripid)) cnt from trips
 group by duration
 order by cnt desc
 limit 1
 
-# 15. which driver, customer pair had more orders
+# 15. which driver, customer pair had more orders ?
 select * from
 (select *, rank() over (order by cnt desc) rnk from
 (select driverid, custid, count(tripid) cnt from trips
 group by driverid, custid)a)b
 where rnk=1
 
-# 16. search to estimate rate
+# 16. search to estimate rate ?
 select sum(searches_got_estimate)/sum(searches) from trips_details
 
-# 17. estimate to search for quote rates
+# 17. estimate to search for quote rates ?
 select sum(searches_for_quotes)/sum(searches_got_estimate) from trips_details
 
-# 18. quote acceptance rate
+# 18. quote acceptance rate ?
 select sum(searches_got_quotes)/sum(searches_for_quotes) from trips_details
 
-# 20. which location got the highest number of trips in each duartion present
+# 20. which location got the highest number of trips in each duartion present ?
 select * from
 (select *, rank() over(partition by duration order by cnt desc) rnk from
 (select duration, loc_from, count(tripid) cnt from trips
 group by duration, loc_from)a)b
 where rnk=1
 
-# 21. which duration got the highest trips in each of the location present
+# 21. which duration got the highest trips in each of the location present ?
 select * from
 (select *, rank() over(partition by loc_from order by cnt desc) rnk from
 (select duration, loc_from, count(tripid) cnt from trips
 group by duration, loc_from)a)b
 where rnk=1
  
-# 22. which area got the highest fares, cancellations,trips,
+# 22. which area got the highest fares, cancellations,trips ?
 select loc_from, sum(fare) fares from trips
 group by loc_from
 order by fares desc
@@ -107,7 +107,7 @@ group by loc_from
 order by cancellations desc
 limit 1
 
-# 23 which duration got the highest trips and fares
+# 23 which duration got the highest trips and fares ?
 select duration, sum(fare) fares from trips
 group by duration
 order by fares desc
